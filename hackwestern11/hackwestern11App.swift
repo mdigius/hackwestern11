b0e22faa5fc9f83a2ee9f835530d27f220b66135
Michael Di Giuseppe <mdigius@uwo.ca>
2024-11-30 08:28:36 -0500

    Initial Commit

---
+++ hackwestern11/hackwestern11App.swift
@@ -0,0 +1,32 @@
+//
+//  hackwestern11App.swift
+//  hackwestern11
+//
+//  Created by Michael Di Giuseppe on 2024-11-30.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct hackwestern11App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ hackwestern11Tests/hackwestern11Tests.swift
@@ -0,0 +1,17 @@
+//
+//  hackwestern11Tests.swift
+//  hackwestern11Tests
+//
+//  Created by Michael Di Giuseppe on 2024-11-30.
+//
+
+import Testing
+@testable import hackwestern11
+
+struct hackwestern11Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
